0de289134f1a6ffa9103e78e9a298b74e9a038f2
Ranoldt <117882837+Ranoldt@users.noreply.github.com>
2024-07-11 12:29:49 -0700

    Part1- read_calls func

--- part1.py
+++ part1.py
@@ -1,6 +1,16 @@
 def read_calls(file: open) -> {(str, str): int}:
-    pass
+    call_dict = {}
+    for line in file:
+        line = line.strip().split(':')
+        for val in line[1:]:
+            call_tuple = (line[0], val)
+            if call_tuple in call_dict:
+                call_dict[call_tuple] += 1
+            else:
+                call_dict[call_tuple] = 1
+    return call_dict
 
 
 def call1to2(calls: {(str, str): int}) -> {str: {str: int}}:
     pass
+
