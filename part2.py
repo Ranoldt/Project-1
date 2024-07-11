657bb5801eff3dfdb10c7bdfcfc5a200c4062828
Ranoldt <117882837+Ranoldt@users.noreply.github.com>
2024-07-11 16:04:34 -0700

    Part2 v1

--- part2.py
+++ part2.py
@@ -1,14 +1,13 @@
 def question1(n: dict) -> dict:
-    pass
+    return {value: key for key, value in n.items()}
 
 
 def question2(n: dict) -> dict:
-    pass
+    return {value: [key for key in n if n[key] == value] for value in n.values()}
 
 
-def question3(n: dict) -> dict:
-    pass
-
+def question3(n: dict, m: dict) -> dict:
+    return {key: n.get(key,0) + m.get(key,0) for key in list(n) + list(m)}
 
 def question4(n: list) -> list:
-    pass
+    return list({element for sublist in n for element in sublist})
