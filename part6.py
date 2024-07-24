2636fe4ef050cbf9e76b05289a578e7aab538d55
Ranoldt <117882837+Ranoldt@users.noreply.github.com>
2024-07-24 16:12:55 -0700

    Part6 v3

--- part6.py
+++ part6.py
@@ -1,8 +1,10 @@
 class PeekableIterator:
     def __init__(self, iterable):
         self.iterator = iter(iterable)
+        self.iterator_double = iter(iterable)
         self.next_peek = None
         self.next_value = None
+        self.count = 1
 
     def __iter__(self):
         return self
@@ -11,6 +13,7 @@ class PeekableIterator:
         if self.next_value is None:
             if self.next_peek is None:
                 self.next_value = next(self.iterator)
+                self.count += 1
                 return self.next_value
             else:
                 self.next_value = self.next_peek
@@ -20,9 +23,8 @@ class PeekableIterator:
             return self.next_value
         else:
             self.next_value = next(self.iterator)
+            self.count += 1
             return self.next_value
-
-
 
     def peek(self):
         if self.next_value is None:
@@ -30,19 +32,20 @@ class PeekableIterator:
             return self.peek_value
         elif self.next_peek is None:
             self.next_peek = next(self.iterator)
+            self.count += 1
             return self.next_peek
         elif self.next_peek > self.next_value:
             return self.next_peek
         else:
             self.next_peek = next(self.iterator)
+            self.count += 1
             return self.next_peek
-
-
 
     def has_next(self):
         try:
-            self.peek()
-            self.next_peek = self.next_value
+            for i in range(self.count):
+                next(self.iterator_double)
+            self.count = 1
             return True
         except StopIteration:
             return False
@@ -52,8 +55,8 @@ if __name__ == "__main__":
     print(next(x))
     print(x.peek())
     print(next(x))
-    print(x.peek())
-    print(x.peek())
     print(x.has_next())
+    print(x.peek())
+    print(x.peek())
     print(next(x))
     print(x.has_next())
