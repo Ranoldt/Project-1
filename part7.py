14e7f1bb83a748e9c4c62751d2a25e951ff17d36
Ranoldt <117882837+Ranoldt@users.noreply.github.com>
2024-07-24 23:00:37 -0700

    part7

--- part7.py
+++ part7.py
@@ -1,2 +1,9 @@
-#Write your commend using python comments
-#Answer:
+# Write your commend using python comments
+# Answer:
+#   Assuming you have created the remote git repository and git installed, the first step in the terminal is to
+#   travel to the directory of the project using cd [the path to the project]. Afterward, type "git init" to initialize
+#   the local repository. To connect to the remote repository, type "git remote add origin [your github git url or ssh]"
+#   Now, you can make commits by adding the files to the staging area using "git add ." and committing using "git commit
+#   -m '[title of commit]'". Finally, pushing from your local repository to your remote using "git push -u origin main"
+#   for your first push. Consequent pushes can use "git push" as the local 'main' branch is already connected.
+
