fad63d837cc7795859fb042364ba0ea0cd198050
Ranoldt <117882837+Ranoldt@users.noreply.github.com>
2024-07-24 22:40:13 -0700

    part2

--- part2.py
+++ part2.py
@@ -6,9 +6,10 @@ def question2(n: dict) -> dict:
     return {value: [key for key in n if n[key] == value] for value in n.values()}
 
 
-def question3(n: dict, m: dict) -> dict:
-    return {key: n.get(key, 0) + m.get(key, 0) for key in list(n) + list(m)}
+def question3(n1: dict, n2: dict) -> dict:
+    return {key: (n1[key] if key in n1 else 0) + (n2[key] if key in n2 else 0) for key in set(list(n1) + list(n2))}
 
 
 def question4(n: list) -> list:
     return list({element for element in [element for sublist in n for element in sublist] if [element for sublist in n for element in sublist].count(element) > 1})
+
